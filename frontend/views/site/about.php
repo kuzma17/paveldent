cfbe05fbb540f1bf034dce1d3620e381d37474dc
kuzma17 <v.kuzma@mail.ru>
2017-01-13 01:46:23 +0200

    test step -1

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -6,7 +6,6 @@ use yii\helpers\Html;
 
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
-$test = 123;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
